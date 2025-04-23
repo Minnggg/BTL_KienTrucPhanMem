2cf8e2195952e3522598f872a370b879d3134378
Bui Huy Hoang <buihuyhoang2709.dev@gmail.com>
2025-04-23 14:34:23 +0700

    auto redirect to login page

--- src/main/java/com/example/BTL_KienTrucPhanMem/controller/AuthController.java
+++ src/main/java/com/example/BTL_KienTrucPhanMem/controller/AuthController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthController {
+    @GetMapping("/")
+    public String gologinForm() {
+        return "redirect:/login"; // login.html trong templates
+    }
+
     @GetMapping("/login")
     public String loginForm() {
         return "login"; // login.html trong templates
